5beb5068919357b6ce55d644db32129511e79dee
beskep <beskep@naver.com>
2025-04-30 10:29:10 +0900

    feat: 전처리

---
+++ eesi/config.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import dataclasses as dc
+import enum
+from pathlib import Path
+
+import cyclopts
+
+
+class BldgType(enum.StrEnum):
+    RESIDENTIAL = '가구'
+    SOCIAL_SERVICE = '사회복지시설'
+
+
+class Vars:
+    YEAR: str = '사업연도'
+    KEY: str = '기준키'
+    CONSTR: str = '시공내역'
+    COST: str = '총지원금액'
+    CONTRACTOR_PAYMENTS: str = '시공업체계약금액'
+
+    # 가구
+    REGISTRATION_DATE: str = '등록일'
+    SUPPORT_TYPE: str = '보호유형'
+
+    # 사회복지시설
+    EXISTING_BOILER: str = '기존보일러유무'
+    EXISTING_BOILER_FUEL: str = '기존보일러연료'
+    EXISTING_BOILER_FUEL_COLS: tuple[str, ...] = (
+        '기름일반',
+        '기름고효율',
+        '가스일반',
+        '가스콘덴싱',
+    )
+
+
+@dc.dataclass
+class Dirs:
+    raw: Path = Path('0000.raw')
+    data: Path = Path('0001.data')
+    analysis: Path = Path('0100.analysis')
+
+
+@cyclopts.Parameter(name='*')
+@dc.dataclass
+class Config:
+    root: Path
+    dirs: Dirs = dc.field(default_factory=Dirs)
+
+    def __post_init__(self):
+        self.update()
+
+    def update(self):
+        for field in (f.name for f in dc.fields(self.dirs)):
+            v = getattr(self.dirs, field)
+            setattr(self.dirs, field, self.root / v)
+
+        return self
+
+
+if __name__ == '__main__':
+    import cyclopts
+    import rich
+
+    conf = cyclopts.config.Toml('config.toml').config
+    rich.print(conf)
---
+++ eesi/s01prep.py
@@ -0,0 +1,161 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, ClassVar
+
+import cyclopts
+import fastexcel
+import polars as pl
+import polars.selectors as cs
+import rich
+from loguru import logger
+
+from eesi import utils
+from eesi.config import BldgType, Config, Vars
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+app = utils.cli.App(
+    config=cyclopts.config.Toml('config.toml', use_commands_as_keys=False)
+)
+
+
+class Preprocess:
+    CONSTRUCTION: ClassVar[dict[str, str]] = {
+        '가': '가구',
+        '곰': '곰팡이',
+        '기': '기타',
+        '냉': '냉방',
+        '단': '단열(벽)',
+        '바': '바닥',
+        '보': '보일러',
+        '진': '진단',
+        '창': '창호',
+        '천': '단열(천장)',
+        '홈': '중문',
+        '벽': '단열(벽)',
+    }
+
+    @staticmethod
+    def convert(src: str | bytes | Path):
+        reader = fastexcel.read_excel(src)
+
+        def read(sheet: str):
+            return (
+                reader.load_sheet_by_name(sheet)
+                .to_polars()
+                .drop('세대주', '생년월일', strict=False)
+                .rename(lambda x: x.strip())
+                .with_columns(pl.lit(int(sheet)).alias(Vars.YEAR))
+            )
+
+        return (
+            pl.concat([read(x) for x in reader.sheet_names], how='diagonal_relaxed')
+            .select(Vars.YEAR, pl.all().exclude(Vars.YEAR))
+            .sort(Vars.YEAR, Vars.KEY)
+        )
+
+    @classmethod
+    def preprocess(cls, src: str | Path):
+        lf = (
+            pl.scan_parquet(src)
+            .with_columns(
+                pl.col(Vars.CONSTR)
+                .str.split('|')
+                .list.eval(pl.element().replace_strict(cls.CONSTRUCTION))
+            )
+            .with_columns(
+                # 2022년 냉방 -> 선풍기 지원
+                pl.when(pl.col(Vars.YEAR) == 2022)  # noqa: PLR2004
+                .then(
+                    pl.col(Vars.CONSTR)
+                    .list.drop_nulls()
+                    .list.eval(pl.element().replace('냉방', '선풍기'))
+                )
+                .otherwise(pl.col(Vars.CONSTR))
+                .alias(Vars.CONSTR)
+            )
+            .with_columns(pl.col(Vars.CONSTR).list.sort())
+        )
+
+        schema = lf.collect_schema()
+
+        if Vars.REGISTRATION_DATE in schema:
+            lf = lf.with_columns(pl.col(Vars.REGISTRATION_DATE).dt.date())
+
+        if Vars.SUPPORT_TYPE in schema:
+            lf = lf.with_columns(
+                # NOTE 보호유형: 데이터 정제 어려움
+                pl.col(Vars.SUPPORT_TYPE).str.replace_all(' ', '').str.split(',')
+            )
+
+        if Vars.EXISTING_BOILER in schema:
+            lf = lf.with_row_index()
+            fuel = (
+                lf.select(['index', *Vars.EXISTING_BOILER_FUEL_COLS])
+                .unpivot(index='index', variable_name=Vars.EXISTING_BOILER_FUEL)
+                .filter(pl.col('value').is_not_null())
+                .drop('value')
+            )
+            lf = (
+                lf.drop(Vars.EXISTING_BOILER_FUEL_COLS)
+                .select(pl.all().exclude(Vars.EXISTING_BOILER), Vars.EXISTING_BOILER)
+                .with_columns(
+                    pl.col(Vars.EXISTING_BOILER).replace_strict(
+                        {'유': True, '무': False}, return_dtype=pl.Boolean
+                    )
+                )
+                .join(fuel, on='index', how='left')
+                .drop('index')
+            )
+
+        return lf
+
+
+@app.command
+def convert(*, samples: int = 1000, conf: Config):
+    """엑셀 -> parquet 변환."""
+    for bldg in BldgType:
+        logger.info(bldg)
+
+        src = conf.dirs.raw / f'0000.{bldg}.xlsx'
+        data = Preprocess.convert(src)
+
+        data.write_parquet(conf.dirs.data / f'0000.{bldg}.parquet')
+
+        sample = samples < data.height
+        data.sample(min(1000, data.height), shuffle=sample).sort(Vars.KEY).write_excel(
+            conf.dirs.data / f'0000.{bldg}{"-sample" if sample else ""}.xlsx',
+            column_widths=120,
+        )
+        rich.print(data)
+
+
+@app.command
+def prep(*, samples: int = 1000, conf: Config):
+    """데이터 전처리."""
+    for bldg in BldgType:
+        logger.info(bldg)
+
+        src = conf.dirs.data / f'0000.{bldg}.parquet'
+        data = Preprocess.preprocess(src).collect()
+
+        data.write_parquet(conf.dirs.data / f'0001.{bldg}.parquet')
+
+        sample = samples < data.height
+        (
+            data.with_columns(cs.by_dtype(pl.List).cast(pl.String))
+            .sample(min(1000, data.height), shuffle=sample)
+            .sort(Vars.KEY)
+            .write_excel(
+                conf.dirs.data / f'0001.{bldg}{"-sample" if sample else ""}.xlsx',
+                column_widths=120,
+            )
+        )
+
+        rich.print(data)
+
+
+if __name__ == '__main__':
+    utils.terminal.LogHandler.set()
+    app()
